f2d0693b616358a9efe2ec230f8db1e30955942f
Phat <phatdroid94@gmail.com>
2022-09-08 21:36:22 +0700

    update sign up

--- app/src/main/java/com/example/demochat07062022/MainActivity.java
+++ app/src/main/java/com/example/demochat07062022/MainActivity.java
@@ -3,7 +3,13 @@ package com.example.demochat07062022;
 import androidx.appcompat.app.AppCompatActivity;
 import io.socket.client.IO;
 import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
 import android.os.Bundle;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.net.URISyntaxException;
 
@@ -21,5 +27,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mSocket.connect();
+        mSocket.on("alert", new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                JSONObject data = (JSONObject) args[0];
+                try {
+                    String alert = data.getString("alert");
+                    if (!alert.isEmpty()) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity.this, alert, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        signUp(new User("demo1@gmail.com", "123456789"));
+    }
+    private void signUp(User user) {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("email", user.getEmail());
+            jsonObject.put("password", user.getPassword());
+            mSocket.emit("sign-up", jsonObject);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
